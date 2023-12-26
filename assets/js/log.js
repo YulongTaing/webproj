745290805c06d4663525126ef83bb688b4bd3bf1
gnol <yulongyt1122@gmail.com>
2023-12-26 23:11:11 +0700

    bart

--- assets/js/log.js
+++ assets/js/log.js
@@ -9,3 +9,17 @@ registerBtn.addEventListener('click', () => {
 loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
 });
+
+function login(){
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    console.log("Login - Username: " + username + " Password: " + password);
+}
+
+function signup(){
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    console.log("Signup - Username: " + username + " Password: " + password);
+}
